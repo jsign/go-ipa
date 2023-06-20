8a3298e60880cd5514468d283c6efb4dabe88b77
Ignacio Hagopian <jsign.uy@gmail.com>
2023-06-20 13:21:17 -0300

    calculate in projective and batch affine after
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- pipfixedbasis/precomp.go
+++ pipfixedbasis/precomp.go
@@ -5,7 +5,9 @@ import (
 	"runtime"
 
 	"github.com/crate-crypto/go-ipa/bandersnatch"
+	"github.com/crate-crypto/go-ipa/bandersnatch/fp"
 	"github.com/crate-crypto/go-ipa/bandersnatch/fr"
+	"github.com/crate-crypto/go-ipa/common/parallel"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -22,17 +24,25 @@ func NewPrecompPoint(point bandersnatch.PointAffine, windowSize int) PrecompPoin
 		windowSize: windowSize,
 		windows:    make([][]bandersnatch.PointAffine, 256/windowSize),
 	}
+
+	windows := make([][]bandersnatch.PointProj, 256/windowSize)
 	group, _ := errgroup.WithContext(context.Background())
 	group.SetLimit(runtime.NumCPU())
 	for i := 0; i < len(res.windows); i++ {
 		i := i
-		base := point
+		var base bandersnatch.PointProj
+		base.FromAffine(&point)
 		group.Go(func() error {
-			res.windows[i] = make([]bandersnatch.PointAffine, 1<<(windowSize-1))
+			windows[i] = make([]bandersnatch.PointProj, 1<<(windowSize-1))
 			curr := base
-			for j := 0; j < len(res.windows[i]); j++ {
-				res.windows[i][j] = curr
+			for j := 0; j < len(windows[i]); j++ {
+				windows[i][j] = curr
 				curr.Add(&curr, &base)
+			}
+			// BatchJacobianToAffineG1(windows[i])
+			res.windows[i] = make([]bandersnatch.PointAffine, 1<<(windowSize-1))
+			for j := range windows[i] {
+				res.windows[i][j].FromProj(&windows[i][j])
 			}
 			return nil
 		})
@@ -69,4 +79,54 @@ func (pp *PrecompPoint) ScalarMul(scalar fr.Element, res *bandersnatch.PointProj
 			}
 		}
 	}
+}
+
+// TODO(jsign): pulled from gnark. DELETE when updating gnark dependency.
+// BatchJacobianToAffineG1 converts points in Jacobian coordinates to Affine coordinates
+// performing a single field inversion (Montgomery batch inversion trick).
+func BatchJacobianToAffineG1(points []bandersnatch.PointProj) []bandersnatch.PointAffine {
+	result := make([]bandersnatch.PointAffine, len(points))
+	zeroes := make([]bool, len(points))
+	accumulator := fp.One()
+
+	// batch invert all points[].Z coordinates with Montgomery batch inversion trick
+	// (stores points[].Z^-1 in result[i].X to avoid allocating a slice of fr.Elements)
+	for i := 0; i < len(points); i++ {
+		if points[i].Z.IsZero() {
+			zeroes[i] = true
+			continue
+		}
+		result[i].X = accumulator
+		accumulator.Mul(&accumulator, &points[i].Z)
+	}
+
+	var accInverse fp.Element
+	accInverse.Inverse(&accumulator)
+
+	for i := len(points) - 1; i >= 0; i-- {
+		if zeroes[i] {
+			// do nothing, (X=0, Y=0) is infinity point in affine
+			continue
+		}
+		result[i].X.Mul(&result[i].X, &accInverse)
+		accInverse.Mul(&accInverse, &points[i].Z)
+	}
+
+	// batch convert to affine.
+	parallel.Execute(len(points), func(start, end int) {
+		for i := start; i < end; i++ {
+			if zeroes[i] {
+				// do nothing, (X=0, Y=0) is infinity point in affine
+				continue
+			}
+			var a, b fp.Element
+			a = result[i].X
+			b.Square(&a)
+			result[i].X.Mul(&points[i].X, &b)
+			result[i].Y.Mul(&points[i].Y, &b).
+				Mul(&result[i].Y, &a)
+		}
+	})
+
+	return result
 }
